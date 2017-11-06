3414da18cb3bfe6d994c2463b7aec7d93cc593e4
Setya Kurniawan <setya.kuniawan@bukalapak.com>
2017-11-06 15:05:05 +0700

    ci test

--- cmd/ministore/main.go
+++ cmd/ministore/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/SetyaK/BL-Onboarding3-Go-package"
 	"github.com/SetyaK/BL-Onboarding3-Go-package/database"
@@ -12,12 +13,24 @@ import (
 func main() {
 	gotenv.Load()
 
-	sess, error := database.NewSession()
-	if error != nil {
-		fmt.Println(error)
-	} else {
-		pRepo := ministore.ProductRepository{Session: sess}
-		_, c := pRepo.GetAll()
-		fmt.Println(c)
+	sess, err := database.NewSession()
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	// m := database.Migration{Session: sess}
+	// _, err = m.Migrate()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	pRepo := ministore.ProductRepository{Session: sess}
+
+	pRepo.Add("sample product", "the sample product of my mini store", 1)
+	_, c, err := pRepo.GetAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(c)
+
 }
