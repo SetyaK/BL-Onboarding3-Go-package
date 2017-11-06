b6027d0b17c1068665101684d0de7e1e89558580
Setya Kurniawan <setya.kuniawan@bukalapak.com>
2017-11-06 15:43:37 +0700

    update test root pwd

--- database/database_mysql_test.go
+++ database/database_mysql_test.go
@@ -11,7 +11,7 @@ func TestNewSessionMysql(t *testing.T) {
 	os.Setenv("DATABASE_ADAPTER", "mysql")
 	os.Setenv("DATABASE_HOST", "localhost")
 	os.Setenv("DATABASE_USERNAME", "root")
-	os.Setenv("DATABASE_PASSWORD", "secret")
+	os.Setenv("DATABASE_PASSWORD", "root")
 	os.Setenv("DATABASE_NAME", "sys")
 	os.Setenv("ENV", "development")
 
